2a5a2cf24b20a5928451caf3390130a2f6bb1268
Joseph Russell <joseph@thegreengrass.net>
2021-06-24 15:29:18 -0400

    Removing trailing slash

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'http://localhost:3000' : 'https://peaceful-journey-07606.herokuapp.com/';
+export const server = dev ? 'http://localhost:3000' : 'https://peaceful-journey-07606.herokuapp.com';
